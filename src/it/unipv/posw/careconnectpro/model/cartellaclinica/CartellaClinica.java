3ed76268b6269f10a124bf8faf39a34a2e3f7bc9
darlismorillo <darlis.morillomendoza01@universitadipavia.it>
2026-02-11 21:40:53 +0100

    Refactpring cartella clinica

--- src/it/unipv/posw/careconnectpro/model/cartellaclinica/CartellaClinica.java
+++ src/it/unipv/posw/careconnectpro/model/cartellaclinica/CartellaClinica.java
@@ -8,21 +8,13 @@ public class CartellaClinica {
     private String idPaziente;
     private LocalDate dataCreazione;
     private Persona paziente;
-//    private List<Monitoraggio> monitoraggi;
     private int idCartellaClinica;
 
     public CartellaClinica(Persona paziente) {
         this.paziente = paziente;
         this.idPaziente = paziente.getCodiceFiscale();
         this.dataCreazione = LocalDate.now();
-//        this.monitoraggi = new ArrayList<>();
     }
-    
-//    public List<Monitoraggio> getMonitoraggi() {
-//        return monitoraggi;
-//    }
-//    
-//    public void setMonitoraggi(List<Monitoraggio> monitoraggi) {this.monitoraggi = monitoraggi;}
     
     public int getIdCartellaClinica() {return idCartellaClinica;}
     public void setIdCartellaClinica(int idCartellaClinica) { this.idCartellaClinica = idCartellaClinica;}
