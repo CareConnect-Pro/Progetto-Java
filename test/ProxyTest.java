41c78c784fb67ccc397e21ca177c90e7b81e49b1
darlismorillo <darlis.morillomendoza01@universitadipavia.it>
2026-02-12 17:40:47 +0100

    Aggiunta PPT

--- test/ProxyTest.java
+++ test/ProxyTest.java
@@ -1,5 +1,6 @@
 import it.unipv.posw.careconnectpro.model.persona.dipendente.Dipendente;
-import it.unipv.posw.careconnectpro.model.persona.TipoUtente;
+import it.unipv.posw.careconnectpro.model.persona.dipendente.FactoryDipendente;
+
 import it.unipv.posw.careconnectpro.model.rsa.ProxyRSA;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,18 +12,18 @@ public class ProxyTest {
 
     @Before
     public void creaDipendenti(){
-        amministratore = new Dipendente("RSSMRA80A01F205Z", "Mario", "Rossi",
+        amministratore = FactoryDipendente.getDipendente("AMMINISTRATORE","RSSMRA80A01F205Z", "Mario", "Rossi",
                 LocalDate.of(1980, 1, 1), "admin@test.it", "333111",
-                "pass123", TipoUtente.AMMINISTRATORE, LocalDate.now());
+                "pass123",  LocalDate.now());
 
-        medico = new Dipendente("BNCGNN90A01F205X", "Gianni", "Bianchi",
+        medico = FactoryDipendente.getDipendente("MEDICO","BNCGNN90A01F205X", "Gianni", "Bianchi",
                 LocalDate.of(1990, 5, 20), "medico@test.it", "333222",
-                 "pass123", TipoUtente.MEDICO, LocalDate.now());
+                 "pass123", LocalDate.now());
 
 
-        dipendenteNuovo = new Dipendente("VRDLUU00A01F205W", "Luca", "Verdi",
+        dipendenteNuovo = FactoryDipendente.getDipendente("Infermiere","VRDLUU00A01F205W", "Luca", "Verdi",
                 LocalDate.of(2000, 10, 10), "luca@test.it", "333333",
-                "pass123", TipoUtente.INFERMIERE, LocalDate.now());
+                "pass123", LocalDate.now());
     }
 
 
